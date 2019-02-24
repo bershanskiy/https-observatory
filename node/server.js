8e6f7e06f75629746d42386ee8e0e34f8d10b977
Christopher Settles <ChristopherRSettles@gmail.com>
2019-02-24 00:27:48 -0600

    Added new endpoint ruleinfo that takes a rulesetid and spits back targets, rules, securecookies, exclusions, and ruleset tests.

--- node/server.js
+++ node/server.js
@@ -33,14 +33,31 @@ const main = async () => {
 	})
 
 	app.get("/ruleinfo?", (req, response) => {
-		let targetName = req.url.replace("/ruleinfo?ruleid=", "")
-		let targetQuery = 'SELECT * FROM `ruleset_targets` WHERE `target` LIKE \'%' + targetName + '%\''
-		let joinQuery = 'SELECT * FROM ruleset_targets INNER JOIN rulesets ON ruleset_targets.rulesetid=rulesets.rulesetid WHERE ruleset_targets.target LIKE \'%' +targetName +'%\';'
-			database.query(joinQuery, []).then((result) => {
-				console.log("Search Query Served. ")
-				response.setHeader("Content-Type", "application/json")
-				response.send(JSON.stringify(result))
-			})
+		let ruleset_id = req.url.replace("/ruleinfo?ruleid=", "")
+		let secureCookiesQuery = 'SELECT * FROM rulesets LEFT JOIN ruleset_securecookies ON ruleset_securecookies.rulesetid=rulesets.rulesetid WHERE rulesets.rulesetid LIKE \'%' + ruleset_id +'%\';'
+		let rulesQuery = 'SELECT * FROM rulesets LEFT JOIN ruleset_rules ON ruleset_rules.rulesetid=rulesets.rulesetid WHERE rulesets.rulesetid LIKE \'%' + ruleset_id +'%\';'
+		let exclusionsQuery = 'SELECT * FROM rulesets LEFT JOIN ruleset_exclussions ON ruleset_exclussions.rulesetid=rulesets.rulesetid WHERE rulesets.rulesetid LIKE \'%' + ruleset_id +'%\';'
+		//targetsQuery also gets the data about if the target supports hsts
+		let targetsQuery = 'SELECT * FROM rulesets LEFT JOIN ruleset_targets ON ruleset_targets.rulesetid=rulesets.rulesetid LEFT JOIN evidence_hsts_preload ON ruleset_targets.target=evidence_hsts_preload.name WHERE rulesets.rulesetid LIKE \'%' + ruleset_id +'%\';'
+		let rulesetTestsQuery = 'SELECT * FROM rulesets LEFT JOIN ruleset_tests ON ruleset_tests.rulesetid=rulesets.rulesetid WHERE rulesets.rulesetid LIKE \'%' + ruleset_id +'%\';'
+		
+		
+		database.query(secureCookiesQuery, []).then((sc_result) => {
+		database.query(rulesQuery, []).then((ru_result) => {
+		database.query(exclusionsQuery, []).then((ex_result) => {
+		database.query(targetsQuery, []).then((tr_result) => {
+		database.query(rulesetTestsQuery, []).then((rt_result) => {
+			let combined_result = {
+				'securecookies': sc_result,
+				'rules': ru_result,
+				'exclusions': ex_result,
+				'tests': rt_result,
+				'targets': tr_result
+			}
+			console.log("All queries completed. ")
+			response.setHeader("Content-Type", "application/json")
+			response.send(JSON.stringify(combined_result))
+		})})})})});
 		})
 
 	// Serve dynamic content from "/search?" API endpoint
